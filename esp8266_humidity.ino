9e1a0da0d94017d02df22dff1143b74cbc99e4c6
mlryckman27 <mlryckman27@gmail.com>
2022-01-05 18:28:29 -0500

    Added HTTP_GET parameter to server.on() call to update page.

--- esp8266_humidity.ino
+++ esp8266_humidity.ino
@@ -1,3 +1,4 @@
+
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
@@ -9,12 +10,13 @@
 #define DHTPIN  D3  // output pin for DHT22 sensor
 #define DHTTYPE DHT22 // sensor is a DHT22 model
 
-
-ESP8266WebServer server(80);    // initialize server with port 80 open
 DHT dht(DHTPIN, DHTTYPE); // initialize a dht sensor object
 
+ESP8266WebServer server(80);    // initialize server with port 80 open
 const char *ssid = "ESP-AP";
 const char *pass = "testpass0?";
+
+float humidity = 0.0;
 
 
 // HTML handlers
@@ -23,45 +25,28 @@ void handleRoot() {
   server.send(200, "text/html", s); // send web page
 }
 
-void handleHumidityLevel() {
-  delay(2000);
-  float humidity = dht.readHumidity();
-  String humidityValue = String(humidity);
-
-  server.send(200, "text/plain", humidityValue);
-  Serial.println(humidity);
-  Serial.println(humidityValue); 
+void handleReadHumidity() {
+  humidity = dht.readHumidity();
+  server.send(200, "text/plain", String(humidity));
+  Serial.println(humidity); 
 }
 
 
 // Setup initial WiFi connection only one time
 void setup() {
-  delay(1000);
   Serial.begin(115200);
+  dht.begin();
 
   WiFi.softAP(ssid, pass);
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.println(myIP);
-
-  server.on("/", handleRoot);
-  server.on("/readHumidity", handleHumidityLevel);
+  Serial.println(WiFi.localIP());
+  
+  server.on("/", HTTP_GET, handleRoot);
+  server.on("/humidity", HTTP_GET, handleReadHumidity);
   server.begin();                         // start the server
-
-  dht.begin();
 }
 
 
 // In endless loop, read humidity, update HTML, and serve to client
 void loop() {
   server.handleClient();
-
-  //delay(2000);                    // DHT22 needs ~2000 ms between reads
-  //float h = dht.readHumidity();  // read humidty from sensor
-  //String hString = String(h);
-
-  //Serial.println(h);
-  
-  //server.send(200, "text/html", hString); // send the humidity data
-  //server.handleClient();
-
 }
--- index.h
+++ index.h
@@ -1,31 +1,36 @@
 const char MAIN_page[] PROGMEM = R"=====(
 <!DOCTYPE html>
 <html>
+
+<head>
+  <title>DHT22 Test</title>
+</head>
+
 <body>
 
 <div>
-<h1> ESP8266 Humidity Sensor </h1>
-	<button type="button" onlcick="updateData()">Update Data</button>
+  <h1> ESP8266 Humidity Sensor </h1>
 </div>
 
 <div>
-	Humidity Level is: <span id="humidityValue">0</span>
+	Humidity Level is: <span id="humidity">NA</span>
 </div>
 
 <script>
-function updateData() {
+
+setInterval(function() {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			document.getElementById("humidityValue").innerHTML = this.responseText;
+			document.getElementById("humidity").innerHTML = this.responseText;
 		}
 	};
-	xhttp.open("GET", "readHumidity", true)
+	xhttp.open("GET", "/humidity", true)
 	xhttp.send();
-}
+}, 2000);
+
 </script>
 
 </body>
 </html>
 )=====";
-
